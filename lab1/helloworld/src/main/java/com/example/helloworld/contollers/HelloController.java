4b3260600df1c7ef22a838bab69c9828ebd92dd8
FilosofDanil <serhio3347@gmail.com>
2023-09-17 14:06:39 +0200

    created HelloController

---
+++ lab1/helloworld/src/main/java/com/example/helloworld/contollers/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.helloworld.contollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
