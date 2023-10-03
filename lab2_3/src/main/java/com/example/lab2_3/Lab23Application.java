018e274fa772711a8c88c7ff462863631508fd43
FilosofDanil <serhio3347@gmail.com>
2023-10-03 17:27:03 +0200

    init an empty lab2_3 project

---
+++ lab2_3/src/main/java/com/example/lab2_3/Lab23Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab23Application.class, args);
+    }
+
+}
